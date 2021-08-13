501f2f1390dfccd86e589820ad9adcba8c0f3e6d
Florent Gatin <florent.gatin@gmail.com>
2021-08-13 16:53:41 +0200

    Update

--- js/APY.js
+++ js/APY.js
@@ -1,10 +1,9 @@
-  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1ut6PZe7GcGd2TBqpVPQcERh2nkhfMNx-TKFj0l8paFU/edit?usp=sharing';
-  
-  function init() {
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1ut6PZe7GcGd2TBqpVPQcERh2nkhfMNx-TKFj0l8paFU/edit?usp=sharing';
+function init() {
     Tabletop.init( { key: publicSpreadsheetUrl,
                      callback: showInfo,
                      simpleSheet: true } )
-  }
+}
 function showInfo(data, tabletop) {
 data.forEach(function(data) {
     cro_apy.innerHTML = data.header.replace(/\n/g, '');
