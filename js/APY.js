890521afd4619b269a44b21b340fddf76618bd37
Florent Gatin <florent.gatin@gmail.com>
2021-08-25 14:09:38 +0200

    Update APY.js

--- js/APY.js
+++ js/APY.js
@@ -1,6 +1,5 @@
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQsgkcjQ5FZ2ufzE7HML6N1o5IHGdcAPgQeK5PkUQrefyZXc12aHA2mCqIA75XspSIo9G5hAd1w46dm/pub?output=csv';
 function init() {
-          Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ33O19IECOG6D5fj7IS65kI2BCcFdfCjA63tFlMIeRXVCfLwIc7VDpLrtdKyDkC49Zt5l4AEDzcq2u/pub?output=csv', {
+          Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vQsgkcjQ5FZ2ufzE7HML6N1o5IHGdcAPgQeK5PkUQrefyZXc12aHA2mCqIA75XspSIo9G5hAd1w46dm/pub?output=csv', {
           download: true,
           header: true,
           complete: function(results) {
